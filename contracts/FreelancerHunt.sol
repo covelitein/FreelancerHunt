7f7928f8ef3408109d8df2f610c960d8db369399
covelitein <abrahamcovenant2004@gmail.com>
2024-03-28 07:26:05 -0700

    updated and tested smart contract functions

--- contracts/FreelancerHunt.sol
+++ contracts/FreelancerHunt.sol
@@ -42,9 +42,37 @@ contract FreelancerHunt {
         freelancer.freelancer = msg.sender;
         freelancer.id = freelancers.length;
         freelancerExist[msg.sender] = true;
-        isDeleted[freelancers.length] = true;
 
         freelancers.push(freelancer);
+    }
+
+    function updateFreelancerDetails(
+        uint id,
+        string memory description,
+        string memory skillSet
+    ) public view {
+        require(freelancers[id].freelancer == msg.sender, "Unauthorized action");
+        require(isDeleted[id] == false, "freelancer record no more available!");
+        require(bytes(description).length > 0, "description is compulsory");
+        require(bytes(skillSet).length > 0, "skill set is compulsory");
+
+        FreelancerStruct memory freelancer = freelancers[id];
+
+        freelancer.description = description;
+        freelancer.skillSet = skillSet;
+    }
+
+    function deleteFreelancer(
+        uint id
+    ) public {
+        require(freelancers[id].freelancer == msg.sender, "Unauthorized action");
+        require(isDeleted[id] == false, "freelancer record already deleted!");
+
+        isDeleted[id] = true;
+    }
+
+    function isFreelancerDeleted(uint id) public view returns (bool) {
+        return isDeleted[id];
     }
 
     function getFreelancers() public view returns(FreelancerStruct[] memory) {
--- test/FreelancerHunt.test.ts
+++ test/FreelancerHunt.test.ts
@@ -1,34 +1,50 @@
-const { expect } = require('chai')
+const { expect } = require("chai");
 
-describe("Contracts", ()=> {
-    let contract : any,
-        result : any,
-        owner : any,
-        freelancer1 : any,
-        freelancer2 : any
+describe("Contracts", () => {
+  let contract: any,
+    result: any,
+    owner: string,
+    freelancer1: string,
+    freelancer2: string;
 
-    const description = "I am a skilled fullstack developer let's connect and bring our ideas to life"
-    const skillSet = "NextJS, ReactJS, NodeJS, Python, GraphQl, Zustand, Docker"
+    const id = 0
 
+  const description =
+    "I am a skilled fullstack developer let's connect and bring our ideas to life";
+  const skillSet = "NextJS, ReactJS, NodeJS, Python, GraphQl, Zustand, Docker";
 
+  const updatedDescription =
+    "I am a skilled fullstack developer let's connect and bring our ideas to life";
+  const updatedSkillSet = "NextJS, ReactJS, NodeJS, Python, GraphQl, Zustand, Docker";
 
-    beforeEach(async()=> {
-        
-        contract = await ethers.deployContract("FreelancerHunt");
-        [owner, freelancer1, freelancer2] = await ethers.getSigners()
-        await contract.waitForDeployment()
-        
+  beforeEach(async () => {
+    contract = await ethers.deployContract("FreelancerHunt");
+    [owner, freelancer1, freelancer2] = await ethers.getSigners();
+    await contract.waitForDeployment();
+  });
 
+  describe("Freelancer", () => {
+    beforeEach(async () => {
+      await contract
+        .connect(freelancer1)
+        .createFreelancer(description, skillSet);
+    });
+
+    it("should confirm freelancer profile setup", async () => {
+      result = await contract.getFreelancer(id);
+      expect(result.freelancer).to.be.equal(freelancer1);
+    });
+
+    it("should confirm updating freelancer record", async()=> {
+        await contract.connect(freelancer1).updateFreelancerDetails(id, updatedDescription, updatedSkillSet)
+        result = await contract.getFreelancer(id)
+        expect(result.description).to.be.equal(updatedDescription)
     })
 
-    describe("Freelancer", ()=> {
-        beforeEach(async()=> {
-            await contract.connect(freelancer1).createFreelancer(description, skillSet)
-        })
-
-        it("should confirm freelancer profile setup", async()=> {
-            result = await contract.connect(freelancer1).getFreelancer(0)
-            expect(result.freelancer).to.be.equal(freelancer1)
-        })
+    it("should confirm deleting a record", async() => {
+        await contract.connect(freelancer1).deleteFreelancer(id)
+        result = await contract.isFreelancerDeleted(id)
+        expect(result).to.be.equal(true)
     })
-})
+  });
+});
